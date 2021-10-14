8a9342f023c9c6031811825aecd192b613c43729
karinapadua <kapadua@gmail.com>
2021-10-13 22:31:03 -0300

    mensagem customizada no app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Desafio feito: Que orgulho de mim!!! :) "
 
 if __name__ == '__main__':
     app.run(debug=True)
