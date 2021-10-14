09718cea86e04f3152faffd04ec4d929a5880918
karinapadua <kapadua@gmail.com>
2021-10-13 22:38:24 -0300

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Desafio feito: Que orgulho de mim!!! :) "
+    return "Desafio feito!"
 
 if __name__ == '__main__':
     app.run(debug=True)
